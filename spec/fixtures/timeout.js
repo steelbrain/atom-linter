1e6a54ac5e80852f81d5e1f7e8921033bb6b598c
steelbrain <de.steelbrain@gmail.com>
2016-02-23 12:00:51 +0000

    :new: Add specs for the new timeout behavior

--- spec/fixtures/timeout.js
+++ spec/fixtures/timeout.js
@@ -2,5 +2,6 @@
 
 setTimeout(function() {
   // We should be killed at 10 seconds, if we survive 15 then it's an error
+  console.log('Impossible')
   process.exit(1)
 }, 15 * 1000)
--- spec/helper-spec.js
+++ spec/helper-spec.js
@@ -10,6 +10,7 @@ const bothFile = path.join(__dirname, 'fixtures', 'both.js')
 const mixedIndentFile = path.join(__dirname, 'fixtures', 'mixedIndent.txt')
 const somethingFile = path.join(__dirname, 'fixtures', 'something.js')
 const stderrFile = path.join(__dirname, 'fixtures', 'stderr.js')
+const timeoutScript = path.join(__dirname, 'fixtures', 'timeout.js')
 const stderrScript = path.join(__dirname, 'fixtures', 'stderr') +
   (process.platform !== 'win32' ? '.sh' : '.bat')
 const testFile = path.join(__dirname, 'fixtures', 'test.txt')
@@ -79,6 +80,19 @@ describe('linter helpers', function () {
         } catch (_) {
           expect(_.message).toContain('not a directory')
         }
+      })
+    })
+
+    describe('the timeout option:', function() {
+      it('times the process out after certain time', function() {
+        waitsForAsync(async function() {
+          try {
+            await helpers.execNode(timeoutScript)
+            expect(false).toBe(true)
+          } catch (_) {
+            expect(_.message).toContain('timed out')
+          }
+        })
       })
     })
 
